adbc7e09de6a60f48925d172fe9b8bf2eeae1f53
Dhanshri Gawale <gawaledhanshri@gmail.com>
2024-03-03 15:36:37 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,19 +6,47 @@ class CityRepository {
             const city = await City.create({ name});
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository leyer");
             throw {error};
         }
 
     }
 
-    async deleteCity(cityId) {
+    async deleteCity(cityId) { 
         try {
             await City.destroy({
                 where: {
                     id: cityID
                 }
             });
+            return true;
         } catch (error) {
+            console.log("Something went wrong in the repository leyer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository leyer");
+            throw {error};
+        }
+
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPK(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository leyer");
             throw {error};
         }
     }
