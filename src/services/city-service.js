0614fbd79df6326a47fd978d7b01e36dd942f4bc
Dhanshri Gawale <gawaledhanshri@gmail.com>
2024-03-07 15:00:35 +0530

    Added filters to get all cities

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -49,9 +49,9 @@ class CityService {
         }
     }
 
-    async getAllCities() {
+    async getAllCities(filter) {
         try {
-            const city = await this.cityRepository.getAllCities();
+            const cities = await this.cityRepository.getAllCities({name: filter.name});
             return cities;
         } catch (error) {
             console.log("Something went wrong at the service leyer");
