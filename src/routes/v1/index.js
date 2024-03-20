7bf9cf638a10eb3d7cd76f9fc7c2efdf9b2c1b84
Dhanshri Gawale <gawaledhanshri@gmail.com>
2024-03-20 23:13:30 +0530

    added common crud classes for basic crud operations

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
+const AirportController = require('../../controllers/airport-controller');
 
 const router = express.Router();
 
@@ -14,5 +15,7 @@ router.post('/flights',FlightController.create);
 router.get('/flights/:id', FlightController.get);
 router.get('/flights', FlightController.getAll);
 router.patch('/flights/:id', FlightController.update);
+
+router.post('/airports', AirportController.create);
 
 module.exports = router;
